0bb2b61f37117fab355fba1e2806c0fc665dbda8
virgil <i@virgil.gr>
2014-03-24 13:38:02 -0700

    removed more unnessecary files

--- consc/mapreduce/MR-recompile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-
-g++ ../consc/MR-mapper.cpp ../consc/t_consc.cpp -O3 -o MR-mapper.o
--- consc/mapreduce/MR-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-hadoop jar /usr/local/hadoop-0.19.0/contrib/streaming/hadoop-0.19.0-streaming.jar -input /todo -output /todone -mapper /consciousness/consciousness -reducer /consciousness/MR-reducer.py
